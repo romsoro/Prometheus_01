618da2510c52ccee04a2dc0cc896ffc68aeff24f
Roman Soroka <roman.w.soroka@gmail.com>
2024-01-18 20:10:43 +0200

    next

--- 04.py
+++ 04.py
@@ -1,7 +1,21 @@
-def addtwo(a, b):
-    added = a + b
-    return a
+def computepay(hours, rate):
+    print(f'In computepay, {hours} hours, and {rate} rate')
 
 
-x = addtwo(2, 7)
-print(x)
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+
+try:
+    h = float(hrs)
+    r = float(rate)
+except:
+    print("Error, please enter numeric input")
+    quit()
+
+computepay(h, r)
+if h <= 40:
+    pay = h * r
+else:
+    pay = 40 * r + ((h - 40) * r * 1.5)
+
+print(pay)
